8b35aca6b46c463f1f6a91a92abdbc40c9301824
Diego <diegorosas19@gmail.com>
2021-04-08 01:36:54 -0300

    desafio 01
    Desafio Aula01 - Conversor de moedas

---
+++ Aula01/Desafio/app.js
@@ -0,0 +1,18 @@
+//var valorEmDolarTexto = prompt("Qual o vvalor em dolar você quer converter?")
+
+//var valorEmDolarNumero = parseInt(valorEmDolarTexto)
+
+//var valorEmReal = valorEmDolarNumero * 5.50
+
+//#000000
+//alert("R$ " + valorEmReal)
+//const input = document.querySelector('input');
+//imprime = function (){ var teste = document.getElementById('valor').value;}
+//input.addEventListener('input', updateValue);
+//var teste = document.getElementById("valor").value;
+
+//var teste2 = teste + 1;
+//console.log(teste);
+//function updateValue(e) {
+  //log.textContent = e.target.value;
+//}
